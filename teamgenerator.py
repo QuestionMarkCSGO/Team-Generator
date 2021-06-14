2145126563c5c5deb6a9e4395aee4d01fefea098
smokR47 <83354742+smokR47@users.noreply.github.com>
2021-06-14 18:58:51 +0200

    added tg_list, config. improved TeamGenerator Class

--- teamgenerator.py
+++ teamgenerator.py
@@ -1,17 +1,26 @@
 import random
+import discord
 
-class TeamGenerator():
+class TeamGenerator:
 
-    def __init__(guild, emb, players):
-        self.guild = guild
-        self.emb = emb
-        self.players = players
+    def __init__(self, msgID, author):
+        self.msgID = msgID      # message id for TeamGenerator embed
+        self.author = author    # creator of TeamGenerator (user who wrote the command) to determine who can close the TeamGenerator
+        self.players = []       # list of players joined TeamGenerator
 
+    # add a player to the list
+    def add_player(self, player):
+        self.players.append(player)
+        print(f'added {player.name}')
 
-    def update_embed(emb):
+    # remove player from list
+    def rem_player(self, player):
+        if player in self.players:
+            remove(player)
+            print(f'removed {player.name}')
+        else:
+            print(f'{player.name} not in players list')
+
+    # update embeds (emb: the current embed, mode: 1 - added / removed a player : 2 - generate teams )
+    def update_embed(self, mode):
         return emb
-
-
-
-
-    
