4c43310b5a8ad080c368238b5d5e954352646011
smokR47 <83354742+smokR47@users.noreply.github.com>
2021-06-15 00:19:56 +0200

    added intents, WIP added on reaction remove, edited tg embed

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import os                           # import os
 import discord                      # import discord
+from discord import Embed
 from discord.ext import commands    # import discord commands
 from config import *                # import config
 from teamgenerator import *         # import teamgenerator class
 
 
+# set intents
+intents = discord.Intents.all()
 
 # create bot
 bot = commands.Bot(command_prefix='.', description='Random Team Generator')
@@ -36,21 +39,38 @@ async def on_ready():
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    id_dict = {}
-    for tg in tg_list:
-        id_dict[tg] = tg.msg.id
-    if reaction.message.id in id_dict:
-        if str(reaction.emoji) == '✅':
-            for tg in tg_list:
-                if tg.msg.id == reaction.message.id:
-                    tg.add_player(user)
-            print(user.name)
-        if str(reaction.emoji) == '🚀':
-            pass
-        if str(reaction.emoji) == '🎤':
-            pass
-        if str(reaction.emoji) == '❌':
-            pass
+    if user != bot.user:
+        debug('Reaction add triggerd from ' + user.name)
+        id_dict = {}
+        for tg in tg_list:
+            id_dict[tg] = tg.msg.id
+        for tg in id_dict:
+            if tg.msg.id == reaction.message.id:
+                if str(reaction.emoji) == '✅':
+                    await tg.add_player(user)
+                if str(reaction.emoji) == '🚀':
+                    pass
+                if str(reaction.emoji) == '🎤':
+                    pass
+                if str(reaction.emoji) == '❌':
+                    pass
+
+
+@bot.event
+async def on_reaction_remove(reaction, user):
+    if user != bot.user:
+        debug('Reaction remove triggerd from ' + user.name)
+        id_dict = {}
+        for tg in tg_list:
+            id_dict[tg] = tg.msg.id
+        for tg in id_dict:
+            if tg.msg.id == reaction.message.id:
+                if str(reaction.emoji) == '✅':
+                    await tg.rem_player(user)
+
+
+def checkReaction(reaction, user):
+    return user != bot.user and (str(reaction.emoji) == '✅')
 
 
 ##############################
@@ -61,17 +81,14 @@ async def on_reaction_add(reaction, user):
 async def teams(ctx):
     debug('Teams command triggerd')
     # create embed
-<<<<<<< Updated upstream
-    color = randColor()
-    emb = discord.Embed(title='', description='Generate Random Teams', color=color)
-=======
-    emb = discord.Embed(title='Team Generator', description='Generate Random Teams', color=discord.Color.random())
->>>>>>> Stashed changes
-    emb.add_field(name='React to this message', value='✅ to join\n🚀 to generate\n🎤 to move players in voice channel\n❌ to close the Team Generator')
-    emb.add_field(name='Players joined:', value='none', inline=False)
+
+    emb = discord.Embed(title='', description='Generate Random Teams', color=discord.Color.random())
+    emb.add_field(name='React to this message', value='✅  to join\n🚀  to generate\n🎤  to move players in voice channel\n❌  to close the Team Generator')
+    emb.add_field(name='__Players joined:__', value='```none```', inline=False)
     emb.set_author(name=bot.user.name, icon_url=str(bot.user.avatar_url))
+
     emb.set_footer(text=f'created by {ctx.author.name}')
-    emb.set_thumbnail(url=ctx.author.avatar_url)
+    emb.set_thumbnail(url=bot.user.avatar_url)
 
     # send embed and write it to msg (for msg id later on)
     msg = await ctx.send(embed=emb)
@@ -82,12 +99,12 @@ async def teams(ctx):
     await msg.add_reaction('❌')
 
     # set shorter names
-    msgID = msg.id
     author = ctx.author
 
-    # create TeamGeneratro instance and write it to tg_list
-    tg = TeamGenerator(msg, author)
+    # create TeamGenerator instance and write it to tg_list
+    tg = TeamGenerator(bot, msg, author)
     tg_list.append(tg)
+
 
 
 # get the token
--- teamgenerator.py
+++ teamgenerator.py
@@ -3,7 +3,8 @@ import discord
 
 class TeamGenerator:
 
-    def __init__(self, msg, author):
+    def __init__(self, bot, msg, author):
+        self.bot = bot          # bot client
         self.msg = msg          # message id for TeamGenerator embed
         self.author = author    # creator of TeamGenerator (user who wrote the command) to determine who can close the TeamGenerator
         self.players = []       # list of players joined TeamGenerator
@@ -11,33 +12,36 @@ class TeamGenerator:
         self.team2 = []         # list of team 2
 
     # add a player to the list
-    def add_player(self, player):
+    async def add_player(self, player):
         if player in self.players:
             print(f'{player.name} already in player list!')
         else:
             self.players.append(player)
-            print(f'added {player.name}')
-            self.update_embed('add')
-
+            print(f'added {player.name}!')
+            await self.update_embed('add', player)
 
     # remove player from list
-    def rem_player(self, player):
+    async def rem_player(self, player):
         if player in self.players:
-            remove(player)
-            print(f'removed {player.name}')
-            self.update_embed('rem')
+            self.players.remove(player)
+            print(f'removed {player.name}!')
+            await self.update_embed('rem', player)
         else:
             print(f'{player.name} not in players list')
 
     # update embeds (emb: the current embed, mode: add / rem - added / removed a player : gen - generate teams )
-    def update_embed(self, mode):
+    async def update_embed(self, mode, player):
+        playerstr = ''
+        for player in self.players:
+            playerstr += player.name + ', '
         if mode == 'add' or 'rem':
-            emb = discord.Embed(title='Team Generator', description='Generate Random Teams', color=discord.Color.random())
+            emb = discord.Embed(title='', description='Generate Random Teams', color=discord.Color.random())
             emb.add_field(name='React to this message', value='✅ to join\n🚀 to generate\n🎤 to move players in voice channel\n❌ to close the Team Generator')
-            emb.add_field(name='Created by:', value=self.author)
-            emb.add_field(name='Players joined:', value=self.players, inline=False)
-            emb.set_thumbnail(url=self.players.len.avatar_url)
-            self.msg.edit(embed=emb)
-        if mode == gen:
+            emb.add_field(name='Players joined:', value=f'```{playerstr[:-2]}```', inline=False)
+            emb.set_author(name=self.bot.user.name, icon_url=str(self.bot.user.avatar_url))
+            emb.set_footer(text=f'created by {self.author.name}')
+            emb.set_thumbnail(url=player.avatar_url)
+            await self.msg.edit(embed=emb)
+        if mode == 'gen':
             pass
         return emb
